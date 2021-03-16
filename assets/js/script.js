593ff3142eaa79abc763137999f9cdca75d22098
Lacey <lacey.griffith@g.austincc.edu>
2021-03-16 12:03:09 -0500

    add style, trash drop ability

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,6 @@ var loadTasks = function () {
 
   // loop over object properties
   $.each(tasks, function (list, arr) {
-    // console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function (task) {
       createTask(task.text, task.date, list);
@@ -152,7 +151,6 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
-  console.log(taskEl);
 };
 
 //make lists sortable
@@ -161,13 +159,24 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function (event) {},
-  deactivate: function (event) {},
-  over: function (event) {},
-  out: function (event) {},
+  activate: function (event) {
+    $(this).addClass("dropover");
+
+    $("#trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function (event) {
+    $(this).removeClass("dropover");
+
+    $("#trash").removeClass("bottom-trash-active bottom-trash-drag");
+  },
+  over: function (event) {
+    $(event.target).addClass("dropover-active")
+  },
+  out: function (event) {
+    $(event.target).removeClass("dropover-active")
+  },
   update: function (event) {
     var tempArr = [];
-
     $(this).children().each(function () {
       var text = $(this)
         .find("p")
@@ -183,8 +192,6 @@ $(".card .list-group").sortable({
         text: text,
         date: date
       });
-
-      console.log(text, date);
     });
 
     var arrName = $(this)
@@ -202,10 +209,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(this).removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
+    $(this).addClass("bottom-trash-active");
   },
   out: function(event, ui) {
+    $(this).removeClass("bottom-trash-active");
   }
 });
 
@@ -222,7 +232,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
